a362f82dacd324afef256741411423acc083b85a
whitbrooks <whitbrooks@gmail.com>
2018-09-13 13:11:58 -0700

    draft, haven't tested

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,126 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
 
-var friendsArray = [];
+var friendsArray = [
+    {
+        "name":"Rachel the Rabbit",
+        "photo":"https://www.comedywildlifephoto.com/gallery/2017_winners.php#&gid=1&pid=16",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }    
+      
+      {
+        "name":"Susie the Seal",
+        "photo":"https://www.comedywildlifephoto.com/gallery/2017_winners.php#&gid=1&pid=17",
+        "scores":[
+            1,
+            1,
+            4,
+            4,
+            3,
+            1,
+            5,
+            5,
+            4,
+            5
+          ]
+      } 
+
+      {
+        "name":"Molly the Mouse",
+        "photo":"https://www.comedywildlifephoto.com/images/gallery/8/00000178_t.jpg",
+        "scores":[
+            3,
+            2,
+            4,
+            5,
+            5,
+            1,
+            2,
+            4,
+            3,
+            1
+          ]
+      } 
+
+      {
+        "name":"Carl the Crab",
+        "photo":"https://www.comedywildlifephoto.com/gallery/2017_finalists.php#&gid=1&pid=14",
+        "scores":[
+            1,
+            1,
+            4,
+            4,
+            5,
+            1,
+            5,
+            5,
+            5,
+            3
+          ]
+      } 
+
+      {
+        "name":"Kevin the Kangaroo",
+        "photo":"https://www.comedywildlifephoto.com/gallery/2017_finalists.php#&gid=1&pid=36",
+        "scores":[
+            4,
+            2,
+            4,
+            1,
+            1,
+            5,
+            3,
+            5,
+            1,
+            1
+          ]
+      } 
+
+      {
+        "name":"Olly the Owl",
+        "photo":"https://www.comedywildlifephoto.com/gallery/2016_winners.php#&gid=1&pid=5",
+        "scores":[
+            5,
+            1,
+            1,
+            4,
+            1,
+            5,
+            2,
+            3,
+            4,
+            2
+          ]
+      } 
+
+      {
+        "name":"Leonard the Lizard",
+        "photo":"https://www.comedywildlifephoto.com/gallery/2016_winners.php#&gid=1&pid=15",
+        "scores":[
+            3,
+            1,
+            2,
+            4,
+            1,
+            1,
+            4,
+            5,
+            5,
+            3
+          ]
+      } 
+
+
+];
   
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,62 +1,53 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// Linking routes to friends data source
 // ===============================================================================
 
-var path = require("path");
-
-var friendsData = require("../data/friends");
-
+var friendsArray = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-//  A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
+  module.exports = function(app){
+    //a GET route that displays JSON of all possible friends
+    app.get('/api/friends', function(req,res){
+      res.json(friendsArray);
+      console.log(friendsArray);
+    });
   
-  app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    // Capture the user input object
-    var userInput = req.body;
-        console.log('userInput = ' + JSON.stringify(userInput));
+    app.post('/api/friends', function(req,res){
+      //grabs the new friend's scores to compare with friends in friendsArray
+      var newFriendScores = req.body.scores;
+      var scoresDifArray = [];
+      var bestMatch = 0;
+  
+      //runs through all friends in friendsArray
+      for(var i=0; i<friendsArray.length; i++){
+        var scoresDif = 0;
+        //runs through new friend scores to compare to existing friends
+        for(var j=0; j<newFriendScores.length; j++){
+          scoresDif += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+        }
+  
+        //push score differences into scoresDifArray
+        scoresDifArray.push(scoresDif);
+      }
+  
+      //loop through scoresDifArray to find best friend match
+      for(var i=0; i<scoresDifArray.length; i++){
+        if(scoresDifArray[i] <= scoresDifArray[bestMatch]){
+          bestMatch = i;
+        }
+      }
+  
+      //return bestMatch data
+      var newFriend = friendsArray[bestMatch];
+      res.json(newFriend);
+  
+      //pushes new submission into the friendsList array
+      friendList.push(req.body);
+    });
+  };
 
-    var userResponses = userInput.scores;
-        console.log('userResponses = ' + userResponses);
-    
-    
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = [];
-    waitListData.length = [];
-
-    res.json({ ok: true });
-  });
 };
